0674f1df2e3efabe0ddd5f1eff7f9b35f238ab7c
kylo_el <kylo.el@gmail.com>
2013-02-15 14:50:13 +0100

    prawie git init

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ git.node
 Object _GIT_ with constructor taking path to repository and methods to manipulate that repository
 vs
 functions for manipulating repos expecting path to repo as argument.
+
+Switch to orion dfs instead of node.js fs?
---
+++ git.node/index.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var ph = require('path')
+
+var init = function(path)
+{
+    var files = ['config', 'description', 'HEAD', 'index'];
+    var folders = ['branches', 'hooks', 'info', ['objects', 'objects/info', 'objects/pack'], , 'refs'];
+    fs.mkdir(path, function (err) {
+    console.log(path);
+        if (err) throw err;
+        files.forEach(function(item) {
+            fs.open(ph.join(path, item), 'w', function (err, fd){
+                console.log(ph.join(path, item));
+                if (err) throw err;
+                fs.close(fd, function (err) {
+                    if (err) throw err;
+                });
+            });
+        });
+        folders.forEach(function(item) {
+            fs.mkdir(ph.join(path, item), function (err) {
+                console.log(ph.join(path, item));
+                if (err) throw err;
+            });
+        });
+    });
+}
+
+var initInPath = function(path)
+{
+    init(ph.join(path, '.git'));
+}
+
+var git = function(pathToRepo)
+{
+    var splitedPath = pathToRepo.split(ph.sep)
+    pathToRepo = ph.resolve(pathToRepo);
+    console.log('1',pathToRepo);
+    if(splitedPath[splitedPath.length-1] === '.git')
+    {
+        console.log('Path to .git was given');
+        this.path = pathToRepo;
+    }
+    else
+    {
+        this.path = ph.join(pathToRepo, '.git');
+        console.log('Path was given without .git');
+    }
+    console.log('2', this.path);
+    var arg = this.path;
+    fs.exists(arg, function (exists) 
+    {
+        exists ? console.log('.git in folder') : console.log('No .git in folder');
+        if(!exists)
+        {
+            console.log('There is no repository here so I am doing git init');
+            init(arg);
+        }
+    });
+    
+    
+}
+git.prototype = 
+{
+    add: function()
+    {
+    
+    }
+
+
+}
+exports.git = git;
+exports.init = initInPath;
